7c7c133ebcdb09572323d5be50be715b9223085b
Hemath DJ <djhemath@gmail.com>
2023-10-27 22:49:21 +0530

    feat: configured global color variable

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
 
 import './index.css'
+import './colors.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
