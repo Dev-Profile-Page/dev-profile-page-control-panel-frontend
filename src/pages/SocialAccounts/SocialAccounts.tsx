26f485bae4f1a5bbe49a914e16747d98212b481c
Hemath DJ <djhemath@gmail.com>
2023-10-27 22:51:31 +0530

    feat: SocialAccounts page

--- src/pages/SocialAccounts/SocialAccounts.tsx
+++ src/pages/SocialAccounts/SocialAccounts.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 
+import styles from './SocialAccounts.module.css';
+import { SocialAccountCard } from '../../components/SocialAccountCard/SocialAccountCard';
+import { Github, Stackoverflow, Twitter } from '../../components/Icons/Icons';
+
 export type SocialAccountsProps = {
 
 };
 
 export function SocialAccounts({}: SocialAccountsProps) {
     return (
-        <h1>SocialAccounts</h1>
+        <div className={styles.page}>
+            <div className={styles['page-title-container']}>
+                <h1>Social Accounts</h1>
+            </div>
+
+            <div className={styles['social-account-cards-wrapper']}>
+                <SocialAccountCard icon={Twitter} name='Twitter' isConnected={true} />
+                <SocialAccountCard icon={Stackoverflow} name='Stackoverflow' isConnected={false} />
+                <SocialAccountCard icon={Github} name='Github' isConnected={true} />
+            </div>
+        </div>
     )
 }
