5eb491c36eb0c13513c03d8e95cb9fc1da6ab9e8
Hemath DJ <djhemath@gmail.com>
2023-11-09 12:00:41 +0530

    feat: add subtitle

--- src/components/PageContentSection/PageContentSection.tsx
+++ src/components/PageContentSection/PageContentSection.tsx
@@ -5,13 +5,21 @@ import styles from './PageContentSection.module.css';
 
 export type PageContentSectionProps = {
   title: string,
+  subtitle?: string,
 } & React.PropsWithChildren;
 
-export function PageContentSection({ title, children }: PageContentSectionProps) {
+export function PageContentSection({ title, subtitle, children }: PageContentSectionProps) {
   return(
     <div className={styles['page-content-section']}>
       <div className={styles['section-container']}>
         <div className={styles['section-title']}>{title}</div>
+        {
+          Boolean(subtitle)
+            ?
+              <span>{subtitle}</span>
+            :
+              null
+        }
       </div>
       <div className={styles['children-container']}>
         {children}
