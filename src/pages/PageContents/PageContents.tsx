ce4ec5ca6c33785b07b880f83a236a35b80a49a4
Hemath DJ <djhemath@gmail.com>
2023-11-09 12:37:34 +0530

    feat: PageContents page

--- src/pages/PageContents/PageContents.tsx
+++ src/pages/PageContents/PageContents.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react';
 
 import styles from './PageContent.module.css';
+import { RoundedButton, RoundedButtonColor, RoundedButtonSize } from '../../components/Buttons/RoudedButton';
+import { Divider } from '../../components/Divider/Divider';
+import { Github, ReactIcon, Stackoverflow, Twitter, TypeScriptIcon } from '../../components/Icons/Icons';
+import { SizedBox } from '../../components/SizedBox/SizedBox';
+import { PageContentSection } from '../../components/PageContentSection/PageContentSection';
+import { TextInput } from '../../components/UserInputs/TextInput';
+import { TextAreaInput } from '../../components/UserInputs/TextAreaInput';
+import { MultiSelectInput, MultiSelectOption } from '../../components/UserInputs/MultiSelectInput';
+import { ToggleInputCard } from '../../components/UserInputs/ToggleInputCard';
 
 export type PageContentsProps = {  };
+
+export const TECHNOLOGIES: MultiSelectOption[] = [
+  { value: 'react', label: 'React', id: 1, icon: ReactIcon },
+  { value: 'typescript', label: 'TypeScript', id: 2, icon: TypeScriptIcon },
+];
 
 export function PageContents({  }: PageContentsProps) {
   return (
@@ -11,7 +25,47 @@ export function PageContents({  }: PageContentsProps) {
           <h1>Page Contents</h1>
       </div>
 
-      
+      <form className={styles['form']}>
+        <PageContentSection title='General'>
+          <TextInput label='Display Name' placeHolder='Type out or click autofill' />
+          <Divider />
+
+          {/* TODO: Convert this into file picker */}
+          <TextInput label='Display picture' placeHolder='Click to select a picture or autofill' />
+          <Divider />
+
+          <TextAreaInput label='Bio' placeHolder='Type out or click autofill' />
+          <Divider />
+
+          <TextInput label='City' placeHolder='Type out' />
+          <Divider />
+
+          <TextInput label='Website' placeHolder='Type out' />
+          <Divider />
+
+        </PageContentSection>
+
+        <PageContentSection title='Cards'>
+          <div className={styles['cards-container']}>
+            <ToggleInputCard id='TWITTER' icon={Twitter} name='Twitter' isSelected={true} onChange={console.log} />
+            <ToggleInputCard id='STACKOVERFLOW' icon={Stackoverflow} name='StackOverflow' isSelected={true} onChange={console.log} />
+            <ToggleInputCard id='GITHUB' icon={Github} name='Github' isSelected={true} onChange={console.log} />
+          </div>
+        </PageContentSection>
+
+        <Divider />
+
+        <PageContentSection title='Technologies' subtitle='Select upto 5'>
+          <MultiSelectInput label='' placeHolder='Type to search' options={TECHNOLOGIES} />
+        </PageContentSection>
+
+        <Divider />
+
+        <div className={styles['save-button-container']}>
+          <RoundedButton backgroundColor={RoundedButtonColor.GREEN} size={RoundedButtonSize.MEDIUM}>Save</RoundedButton>
+        </div>
+      </form>
+      <SizedBox width='auto' height={50} />
     </div>
   );
 }
