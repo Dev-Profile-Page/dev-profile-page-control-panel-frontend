1de3a2c606a0e97d704db9295117913db84cf14f
Hemath DJ <djhemath@gmail.com>
2023-11-09 10:35:53 +0530

    feat: empty ToggleInputCard component

---
+++ src/components/UserInputs/ToggleInputCard.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import commonStyles from './CommonInput.module.css';
+import styles from './ToggleInputCard.module.css';
+
+export type ToggleInputCardProps = { };
+
+export function ToggleInputCard({  }: ToggleInputCardProps) {
+  return (
+    <div></div>
+  );
+}
