668c75268556936363d801bc00f8e7c5eb26f85d
Hemath DJ <djhemath@gmail.com>
2023-11-11 01:29:20 +0530

    feat: AuthButton component

---
+++ src/components/AuthButton/AuthButton.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import styles from './AuthButton.module.css';
+
+export type AuthButtonProps = {
+  id: any,
+  icon: React.ComponentType,
+  name: string,
+  onClick: (id: any) => void,
+};
+
+export function AuthButton({ icon: Icon, id, name }: AuthButtonProps) {
+  return (
+    <div className={styles['auth-button-card-container']}>
+      <div>
+        <Icon />
+        <span>
+          Continue with <label htmlFor={id} className={styles['auth-button-name']}>{name}</label>
+        </span>
+      </div>
+    </div>
+  );
+}
